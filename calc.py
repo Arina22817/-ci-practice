4677fba93cd9814b741e151eb80105f59193790e
student <student@localhost.localdomain>
2026-04-14 19:31:21 +0300

    Баг: добавлен тест на пустой список

--- calc.py
+++ calc.py
@@ -4,3 +4,7 @@ def average(numbers):
 if __name__ == "__main__":
     test_data = [10, 20, 30, 40]
     print(f"Среднее: {average(test_data)}")
+
+def average(numbers):
+    # Ошибка: если список пуст, будет ZeroDivisionError
+    return sum(numbers) / len(numbers)
